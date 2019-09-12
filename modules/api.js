d9aa183628b1a2a84e0d1840f4ceb6dba6a78ef6
Vimukthi Mayadunne <vimukthimayadunne@gmail.com>
2019-09-12 16:00:36 +0530

    git commit all

--- modules/api.js
+++ modules/api.js
@@ -19,11 +19,23 @@ let ApiSchema = new Schema({
             type: Object,
         },
         schemes:{
-            type: Object,
+            type: [String],
         },
         paths: {
             type: Object,
+        customAuth:{
+            name:String ,// "WSO2 APIM getway Acsess gearator"
+            grantType:String, // client_credentias
+            parameters:[{
+                name:String, // clientKey
+                in:String, // body
+                required:Boolean, //true
+                type:String // string
+            }],
+            url:String, //http://172.17.0.2:8280/token
+            description:String // "Genarates Accses token from client credentials to acsess data"
       }
+    }
 });
 
 const Api =mongoose.model('api', ApiSchema);
